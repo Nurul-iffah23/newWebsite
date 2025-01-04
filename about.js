2b81d8cb71680eac09ac7680ece4ff588dd59e0f
Nurul-iffah23 <nuruliffahhumaira@gmail.com>
2025-01-04 12:03:02 +0800

    Delete about.js

--- about.js
+++
@@ -1,5 +0,0 @@
-// This script can be extended if you want to add dynamic features like member profiles or animations.
-document.addEventListener("DOMContentLoaded", () => {
-    console.log("Page loaded successfully!");
-  });
-  
