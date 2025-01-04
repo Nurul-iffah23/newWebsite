22ac8d40acfef4dae0b89f7818b475f0e78205eb
Nurul-iffah23 <nuruliffahhumaira@gmail.com>
2025-01-05 01:18:37 +0800

    go

--- script1.js
+++ script1.js
@@ -4,4 +4,7 @@ function toggleMenu() {
     const navLinks = document.querySelector('.nav-links');
     navLinks.classList.toggle('active');
 }
+document.querySelector('.hamburger').addEventListener('click', () => {
+    document.querySelector('.navbar').classList.toggle('active');
+});
 
